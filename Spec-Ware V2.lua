6c80ef93bb57a39a0c7e4e18d2532b50a24f048c
kyle1232213 <129571960+kyle1232213@users.noreply.github.com>
2023-10-15 08:16:29 -0500

    Create Spec-Ware V2.lua

---
+++ Spec-Ware V2.lua
@@ -0,0 +1,15 @@
+--[[ 
+
+blud really thought I was finna give him a free script already :/
+blud really thought I was finna give him a free script already :/
+blud really thought I was finna give him a free script already :/
+blud really thought I was finna give him a free script already :/ 
+
+]]--
+
+
+
+local plr = game.Players.LocalPlayer
+
+plr:kick("blud really thought I was finna give him a free script already :/")
+
